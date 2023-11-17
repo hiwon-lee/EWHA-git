9d54e36d2bf4fe1ad32d60ee698ef1c8d1fa9fd0
hiwon-lee <leather2790@naver.com>
2023-11-17 12:46:20 +0900

    Feat 1330

---
+++ 1330.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    string result;
+
+    if (a > b)
+    {
+        result = ">";
+    }
+    else if (a < b)
+    {
+        result = "<";
+    }
+    else
+    {
+        result = "==";
+    }
+    cout << result;
+}
