29d55d24fd1e54831ba9139a729786f71c96f798
hiwon-lee <leather2790@naver.com>
2023-11-27 02:02:39 +0900

    Feat atm 다풂

--- 11399.cpp
+++ 11399.cpp
@@ -1,10 +1,27 @@
 // ATM
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int n;
+    int n, rst = 0;
+    cin >> n;
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int turn = 0;
+    while (n != turn)
+    {
+        rst += arr[turn] * (n - turn++);
+    }
+
+    cout << rst;
 }
