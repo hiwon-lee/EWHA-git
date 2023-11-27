9e6af14b5e61a38a3a383af565deae52c2d8e3f0
hiwon-lee <leather2790@naver.com>
2023-11-27 14:18:20 +0900

    Feat 한수 시도중 디피필요

---
+++ 1065.cpp
@@ -0,0 +1,37 @@
+// 한수
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool isLimit(int limit, int curr)
+{
+    bool limit = false;
+    if (limit < curr)
+        true;
+    return limit;
+}
+
+// dp 문제 푸는것마냥 풀어야 돼
+int main()
+{
+    int n, cnt = 0;
+    cin >> n;
+    vector<int> arr(n);
+
+    // 몆자리 수인가
+    int volumn = n / 10;
+
+    while (volumn)
+    {
+
+        // int diff =
+
+        if (isLimit)
+        {
+            break;
+        }
+    }
+
+    cout << cnt;
+}
