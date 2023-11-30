0446459ccc0827f243dac8d7fce7cb112fb5fc93
hiwon-lee <leather2790@naver.com>
2023-11-30 16:31:39 +0900

    feat 설탕배달 풂

---
+++ 2839.cpp
@@ -0,0 +1,42 @@
+// 설탕배달
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, rst = 0;
+
+    cin >> n;
+
+    int max = n / 5;
+
+    int rest;
+
+    if (n <= 5)
+    {
+        if (n == 3 || n == 5)
+            cout << 1;
+        else
+            cout << -1;
+    }
+    else
+    {
+        while (true)
+        {
+            rest = n - max * 5;
+            if (rest % 3 == 0)
+            {
+                cout << max + rest / 3;
+                break;
+            }
+            if (max == 0)
+            {
+                cout << -1;
+                break;
+            }
+            max--;
+        }
+    }
+}
