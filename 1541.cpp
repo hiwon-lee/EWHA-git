d2735a3a5e920bc9180b259486f57418d4f36188
hiwon-lee <leather2790@naver.com>
2023-12-01 15:10:30 +0900

    update god damn,,,....,.,.,.

--- 1541.cpp
+++ 1541.cpp
@@ -4,19 +4,36 @@
 #include <vector>
 #include <cmath>
 using namespace std;
+int getNum(string currNumString)
+{
+    int getnum = 0;
+    int idx = 0;
+    for (int i = currNumString.length() - 1; i >= 0; i--)
+    {
+        // cout << "calc" << currNumString[idx++] << endl;
+        // cout << "getnum1 :" << getnum << endl;
 
+        getnum += (currNumString[idx++] - 48) * pow(10, i);
+        // cout << "getnum2 :" << getnum << endl;
+    }
+
+    return getnum;
+}
 int main()
 {
     // TODO : 먼저 입력을 어떻게 받을 지 생각
     // 1. 문자열로 받음?
     // 2. 숫자랑 기호를 번갈아담아
     string input;
+    int rst = 0;
     vector<int> number;
     vector<char> operation;
 
     cin >> input;
 
     string currNumString = "-1";
+    int tempNum = 0;
+    bool isPrevMinus = false;
 
     for (auto &element : input)
     {
@@ -29,27 +46,50 @@ int main()
             else
             { // 정의인 경우
                 currNumString += element;
+                cout << currNumString << endl;
             }
         }
         else
-        { // 숫자가 아닌 경우
-            if (currNumString == "-1")
-            { // 숫자를 벡터에 삽입한 경우, 연산자
-                operation.push_back(element);
+        { // 연산자인 경우
+            cout << "temp num" << tempNum << endl;
+            currNumString = "-1";
+            if (element == '+')
+            {
+                tempNum += getNum(currNumString);
+                isPrevMinus = false;
             }
             else
-            { // 정의인 경우
-                // stoi : 문자열을 숫자로 변환
-                int getnum = 0;
-                int idx = 0;
-                for (int i = currNumString.length() - 1; i >= 0; i--)
-                {
-                    getnum += currNumString[idx++] * pow(10, i);
+            {
+                if (!isPrevMinus)
+                { // 이전에 마이너스가 아닌경우
+                    rst += tempNum;
+                    // currNumString = "-1";
+
+                    // tempNum = 0;
+                    isPrevMinus = true;
+                    cout << "rst after" << rst << endl;
                 }
-                // currNumString = stoi(currNumString);
-                number.push_back(getnum);
-                getnum = 0;
+                else
+                {
+                    // tempNum += getNum(currNumString);
+                    // currNumString = "-1";
+
+                    rst -= tempNum;
+                    isPrevMinus = false;
+                    cout << "rst prevminus " << rst << endl;
+                }
+                tempNum = 0;
                 currNumString = "-1";
+                isPrevMinus = true;
             }
         }
     }
+    tempNum += getNum(currNumString);
+    cout << "final" << tempNum << endl;
+
+    if (isPrevMinus)
+        rst -= tempNum;
+    else
+        rst += tempNum;
+    cout << rst;
+}
