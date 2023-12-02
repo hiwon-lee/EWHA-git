7cfd696145a0b43f99320ac772a6d7added27ac8
hiwon-lee <leather2790@naver.com>
2023-12-02 17:51:54 +0900

    feat 수들의 합 성공

--- 1789.cpp
+++ 1789.cpp
@@ -6,4 +6,29 @@ using namespace std;
 
 int main()
 {
+    long s;
+    cin >> s;
+
+    long element = 1;
+
+    while (true)
+    {
+        if (s - element <= element)
+        {
+            cout << element;
+            break;
+        }
+        else
+        {
+            s -= element++;
+            if (s == 0)
+            {
+                cout << element;
+                break;
+            }
+        }
+        // s = s - element;
+    }
+
+    // cout << 200 - (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18);
 }
